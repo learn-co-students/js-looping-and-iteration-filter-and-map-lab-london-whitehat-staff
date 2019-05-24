9b671bad90c2ee09fa3476b7b32bf432b58e5b54
Kelly <kelly.ho@sky.uk>
2019-05-24 15:45:32 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue) {
+  let driverNames = drivers.filter(driver => driver.revenue > revenue)
+  return driverNames;
+};
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(driver => {
+    return driver.name
+  });
+};
+
+//this needs two returns because the statement cannot be done in one return. we must return the initial value, then we can use that to get the next value. this needed to be done in one function to pass the test.
+
+function exactMatch(drivers, array) {
+  return drivers.filter(driver => {
+    for(key in array) {
+      if(driver[key] === array[key]){
+        return true;
+      };
+    };
+  });
+};
+
+function exactMatchToList(drivers, array) {
+  return exactMatch(drivers, array).map(driver => {
+    return driver.name;
+  });
+};
